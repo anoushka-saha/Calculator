2437544b40d101b31fa6030555770dc35531d58f
anoushka1111 <saha.anoushka@gmail.com>
2024-05-09 08:22:22 -0400

    first round

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+#Anoushka Saha
+#Calculator
+#May 9th, 2024
+#Day 10 Final Project
+
+#Addition function
+def add(int1, int2):
+    return int1 + int2
+
+#Subtraction function
+def subtract(int1, int2):
+    return int1 - int2
+
+#Multiplication function
+def multi(int1, int2):
+    return int1 * int2
+
+#Division function
+def div(int1, int2):
+    return int1 / int2
+
+#Ask user for the first number
+num1 = int(input("What is the first number?: "))
+
+#Ask user for the second number
+num2 = int(input("What is the second number?: "))
+
+#Ask user what operation they want
+opp = input("Pick an operation (+ - * /): ")
+
