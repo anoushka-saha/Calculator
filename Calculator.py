d51e88b1a68c262586690f28c430d28d168d0523
anoushka1111 <saha.anoushka@gmail.com>
2024-05-09 08:49:15 -0400

    adding while loop for repition

--- Calculator.py
+++ Calculator.py
@@ -30,11 +30,34 @@ operations = {
 #Ask user for the first number
 num1 = int(input("What is the first number?: "))
 
-#Ask user for the second number
-num2 = int(input("What is the second number?: "))
+#While loop so user can use again
+while True: 
 
-#Ask user what operation they want
-for sym in operations:
-    print(sym)
-opp = input("Choose an operation from the ones above: ")
+    #Ask user for the second number
+    num2 = int(input("What is the second number?: "))
+
+    #Ask user what operation they want
+    for sym in operations:
+        print(sym)
+    opp = input("Choose an operation from the ones above: ")
+
+    #Finding the answer of user's chosen operation
+    calc = operations[opp]
+    ans = calc(num1, num2)
+
+    #Printing final answer
+    print(str(num1) + " " + opp + " " + str(num2) + " = " + str(ans))
+
+    #Asking user if they want to continue, and if they want to use the same number
+    again = input("Do you want to go again? (y/n): ")
+
+    if again != "y":
+        break
+    else:
+        same_num = input("Type \"y\" if you want to continue with " + str(ans) + " or \"n\" if you want to use a new number: ")
+        if same_num == "n":
+            #Ask user for the first number
+            num1 = int(input("What is the first number?: "))
+        else:
+            num1 = ans
 
